2d8195533c79cfcf8be070d1074ef6bbadd4162d
Mostafa Kanaan <mos.kan@hotmail.com>
2019-10-18 20:36:29 +0200

    Fixed Impressum

--- M1/Impressum.php
+++ M1/Impressum.php
@@ -2,25 +2,10 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="refresh" content="3; URL=Start.html">
+    <meta http-equiv="refresh" content="3; URL=Start.php">
     <title>eMensa - Impressum</title>
 </head>
 <body>
-
-<!--Header bzw Nav include -->
-<?php
-include 'inc/navbar.html'
-?>
-
-
-
-        <!-- Footer -->
-        <?php
-include 'inc/footer.html'
-?>
-
-<?php
-include 'inc/js.html' 
-?>
+<p>Sie werden in Kürze weitergeleitet..</p>
 </body>
 </html>
