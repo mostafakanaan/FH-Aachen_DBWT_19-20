af62b298c44b36cf69b0af569689a720199b2bfc
Marko Kozlowski <marko.kozlowski@alumni.fh-aachen.de>
2019-11-22 19:37:31 +0100

    Filter WIP

--- M3/Produkte.php
+++ M3/Produkte.php
@@ -1,9 +1,6 @@
 <?php require __DIR__ . '/vendor/autoload.php';
-//Connectiion string..
-$query = 'SELECT Mahlzeiten.id, Mahlzeiten.Name,Mahlzeiten.Beschreibung,Mahlzeiten.Vorrat,Mahlzeiten.Verfuegbar, P.`Alt-Text`,P.Titel,P.Binaerdaten
-FROM Mahlzeiten INNER JOIN Mahlzeit_hat_Bild B on Mahlzeiten.ID = B.Mahlzeiten_ID INNER JOIN Bilder P on P.ID = B.Bild_ID;'; //Query um an die Zutaten zu kommen
+//Connection string..
 
-$katequery = 'SELECT * FROM `Kategorien;';
 //Connectiion string..
 $dotenv = Dotenv\Dotenv::create(__DIR__,'.env');
 $dotenv->load();
@@ -15,15 +12,36 @@ $connection = mysqli_connect(
     getenv('DB_NAME'),
     (int) getenv('DB_PORT')
 );
+//*************
+//GET Parameter
+//**********
+$limit = @$_GET['limit'] ?: '8';//Limit GET Parameter, wenn kein Limit angegeben ist der Default wert 8
+$available = @$_GET['avail'] ?: 'false';
+$kat = @$_GET['kat'] ?: 0;
+$vegetarisch = @$_GET['vegetarisch'] ?: 'false';
+$vegan = @$_GET['vegan'] ?: 'false';
 
+
+//*************
+//QUERYS
+//**********
+
+$query = 'SELECT Mahlzeiten.id, Mahlzeiten.Name,Mahlzeiten.Beschreibung,Mahlzeiten.Vorrat,Mahlzeiten.Verfuegbar, P.`Alt-Text`,P.Titel,P.Binaerdaten
+FROM Mahlzeiten INNER JOIN Mahlzeit_hat_Bild B on Mahlzeiten.ID = B.Mahlzeiten_ID INNER JOIN Bilder P on P.ID = B.Bild_ID'; //Query um an die Zutaten zu kommen
+
+if($kat > 0 ){//für alle Anzeigen...
+    $query .= ' WHERE Kategorie_ID = '. $kat .';';
+}
+
+$katequery = 'SELECT * FROM `Kategorien;';
 //Erros abfangen...
 if(mysqli_connect_errno()){
     printf("Verbindung zur Datenbank konnte nicht hergestellt werden: %s\n", mysqli_connect_error());
 }
 $result = mysqli_query($connection, $query);
 $kateresult = mysqli_query($connection, $katequery);
-$limit = @$_GET['limit'] ?: '8';//Limit GET Parameter, wenn kein Limit angegeben ist der Default wert 8
-$available = @$_GET['avail'] ?: 'false';
+
+
 ?>
 
 <!DOCTYPE html>
@@ -54,37 +72,42 @@ include 'inc/navbar.php'
         <div class="col-3">
             <div class="card background" id="bigcard">
                 <div class="card-body">
-                    <h5 class="card-title">Speisenliste filtern</h5>
-                    <div class="form-group">
-                        <select class="form-contro align-items-center" id="kategorien">
-                          <?php
-                          if ($kateresult) {// Query ausführen..
-                              $optgrouprow = mysqli_fetch_all($kateresult); //$optgroup index 0 -> ID 1-> Kategorie_ID 2 -> BILD ID 3 -> Bezeichnung
-                              foreach($optgrouprow as $optgroup) {
-                                  if($optgroup[1] == NULL )//Ab in ein OPTGroup
-                                      echo '<optgroup label="'. $optgroup[3] .'">';
-                                      foreach($optgrouprow as $option) {//Suche alle die zu der OPTGroup gehören...
-                                              if($optgroup[0] == $option[1]){//$row -> Obergruppe $rows -> Subgruppen
-                                                  echo '<option>'. $option[3] .'</option>';
+                    <form action="Produkte.php" method="get">
+
+                        <h5 class="card-title">Speisenliste filtern</h5>
+                        <div class="form-group">
+                            <select class="form-contro align-items-center" id="kategorien">
+    <!--                            Form für Filter...-->
+                                <?php
+                              if ($kateresult) {// Query ausführen..
+                                  $optgrouprow = mysqli_fetch_all($kateresult); //$optgroup index 0 -> ID 1-> Kategorie_ID 2 -> BILD ID 3 -> Bezeichnung
+                                                                                //same for $option
+                                  foreach($optgrouprow as $optgroup) {
+                                      if($optgroup[1] == NULL )//Ab in ein OPTGroup
+                                          echo '<optgroup label="'. $optgroup[3] .'">';
+                                          foreach($optgrouprow as $option) {//Suche alle die zu der OPTGroup gehören...
+                                                  if($optgroup[0] == $option[1]){//$optgroup -> Obergruppe $option -> Subgruppen
+                                                      echo '<option>'. $option[3] .'</option>';
+                                                  }
                                               }
-                                          }
-                                  echo '</optgroup>';//Wenn alle unterkategorien gefunden wurden..
+                                      echo '</optgroup>';//Wenn alle unterkategorien gefunden wurden..
+                                  }
                               }
-                          }
-                          ?>
-                        </select>
-                    </div>
-                    <div class="form-check">
-                        <ul style="list-style: none">
-                            <li><input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
-                                <label class="form-check-label" for="defaultCheck1">nur verfügbare</label></li>
-                            <li><input class="form-check-input" type="checkbox" value="" id="defaultCheck2">
-                                <label class="form-check-label" for="defaultCheck2">nur vegetarische</label></li>
-                            <li><input class="form-check-input" type="checkbox" value="" id="defaultCheck3">
-                                <label class="form-check-label" for="defaultCheck3">nur vegane</label></li>
-                        </ul>
+                              ?>
+                            </select>
+                        </div>
+                        <div class="form-check">
+                            <ul style="list-style: none">
+                                <li><input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+                                    <label class="form-check-label" for="defaultCheck1">nur verfügbare</label></li>
+                                <li><input class="form-check-input" type="checkbox" value="" id="defaultCheck2">
+                                    <label class="form-check-label" for="defaultCheck2">nur vegetarische</label></li>
+                                <li><input class="form-check-input" type="checkbox" value="" id="defaultCheck3">
+                                    <label class="form-check-label" for="defaultCheck3">nur vegane</label></li>
+                            </ul>
                     </div>
                     <a href="#" class="btn btn-dark" id="filter">Speisen filtern</a>
+                    </form>
                 </div>
             </div>
         </div>
