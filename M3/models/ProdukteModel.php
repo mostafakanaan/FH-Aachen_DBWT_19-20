cf3dc4753420342851308fdd3408155bd81c09c2
Marko Kozlowski <marko.kozlowski@alumni.fh-aachen.de>
2019-12-05 19:45:10 +0100

    Filter fixed

--- M3/models/ProdukteModel.php
+++ M3/models/ProdukteModel.php
@@ -7,7 +7,7 @@ namespace Emensa\Model {
     $limit = @$_GET['limit'] ?: '8';
 //Limit GET Parameter, wenn kein Limit angegeben ist der Default wert 8
     $available = @$_GET['avail'] ?: '0';
-    $kat = @$_GET['kat'] ?: 0;
+    $kat = @$_GET['kat'] ?: '0';
     $vegetarisch = @$_GET['vegetarisch'] ?: '0';
     $vegan = @$_GET['vegan'] ?: '0';
 //*************
@@ -17,8 +17,8 @@ namespace Emensa\Model {
     FROM Mahlzeiten INNER JOIN Mahlzeit_hat_Bild B on Mahlzeiten.ID = B.Mahlzeiten_ID INNER JOIN Bilder P on P.ID = B.Bild_ID
     INNER JOIN Mahl_enthaelt_zutat MEZ on Mahlzeiten.ID = MEZ.Mahlzeit_ID
     INNER JOIN Kategorien K on Mahlzeiten.Kategorie_ID = K.ID WHERE Mahlzeiten.Name IS NOT NULL '. ($available ? ' AND Mahlzeiten.Verfuegbar = 1 ' : '') . // Vefügbar abfrage
-         ($vegan ? ' AND 0 = ALL (SELECT  Zutaten.Vegan FROM Zutaten Zutaten WHERE Zutaten.ID = MEZ.Zutat_ID)' : '') . //vegan abfrage
-         ($vegetarisch ? ' AND 0 = ALL (SELECT  Zutaten.Vegan FROM Zutaten Zutaten WHERE Zutaten.ID = MEZ.Zutat_ID)' : '') . //Vegetarisch abfrage
+         ($vegan ? ' AND 1 = ALL (SELECT  Zutaten.Vegan FROM Mahl_enthaelt_zutat INNER JOIN Zutaten on Mahl_enthaelt_zutat.Zutat_ID = Zutaten.ID WHERE Mahl_enthaelt_zutat.Mahlzeit_ID = Mahlzeiten.id)' : '') . //vegan abfrage
+         ($vegetarisch ? ' AND 1 = ALL (SELECT  Zutaten.Vegetarisch FROM Mahl_enthaelt_zutat INNER JOIN Zutaten on Mahl_enthaelt_zutat.Zutat_ID = Zutaten.ID WHERE Mahl_enthaelt_zutat.Mahlzeit_ID = Mahlzeiten.id)' : '') . //Vegetarisch abfrage
          ($kat != 0 ? ' AND K.ID =' . $kat : '') .  ' GROUP BY Mahlzeiten.id' . ($limit != 0 ? ' LIMIT ' . $limit : ''); // Kategorie abfrage -> Group By um mehrfache Einträge zusammen zu fassen..
 
     $katequery = 'SELECT * FROM `Kategorien;'; // Um an alle Kategorien ran zu kommen...
