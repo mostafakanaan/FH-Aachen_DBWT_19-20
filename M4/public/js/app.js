e9de5b3ff20fcebb2aec51654bc28f096863f95a
mostafakanaan <muus.kaan@hotmail.com>
2020-01-09 23:08:30 +0100

    Fixed tabs (js)

--- M4/public/js/app.js
+++ M4/public/js/app.js
@@ -1,11 +1,19 @@
 const beschreibungTab = document.getElementById("home-tab");
 const zutatenTab = document.getElementById("zutaten-tab");
 const bewertungenTab = document.getElementById("bewertungen-tab");
+const beschreibungCont = document.getElementById("beschreibung");
+const zutatenCont = document.getElementById("zutaten");
+const bewertungenCont = document.getElementById("bewertungen");
+
+
 
 beschreibungTab.addEventListener('click', function () {
     beschreibungTab.classList.add("active");
     zutatenTab.classList.remove("active");
     bewertungenTab.classList.remove("active");
+    beschreibungCont.classList.add("active");
+    zutatenCont.classList.remove("active");
+    bewertungenCont.classList.remove("active");
 
 })
 
@@ -13,11 +21,17 @@ zutatenTab.addEventListener('click', function () {
     beschreibungTab.classList.remove("active");
     zutatenTab.classList.add("active");
     bewertungenTab.classList.remove("active");
-
+    beschreibungCont.classList.remove("active");
+    bewertungenCont.classList.remove("active");
+    zutatenCont.classList.add("active");
 })
 
 bewertungenTab.addEventListener('click', function () {
     beschreibungTab.classList.remove("active");
     zutatenTab.classList.remove("active");
     bewertungenTab.classList.add("active");
+    zutatenCont.classList.remove("active");
+    beschreibungCont.classList.remove("active");
+    bewertungenCont.classList.add("active");
 })
+
