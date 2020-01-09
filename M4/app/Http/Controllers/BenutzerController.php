127da78b0e1abc994f50b0110e9ccb757a3cc7b7
Marko Kozlowski <marko.kozlowski@alumni.fh-aachen.de>
2020-01-09 22:45:17 +0100

    Login/Logout erros WIP

--- M4/app/Http/Controllers/BenutzerController.php
+++ M4/app/Http/Controllers/BenutzerController.php
@@ -5,30 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
-class DetailsController extends Controller
+class BenutzerController extends Controller
 {
-    public function index($id)
-    {
-        session_start();
-        $_SESSION['error'] = false;
-        $_SESSION['angemeldet'] = 0;
-
-      $mahlzeiten =  DB::table('Mahlzeiten')
-            ->join('Mahlzeit_hat_Bild','Mahlzeiten.ID','=','Mahlzeit_hat_Bild.Mahlzeiten_ID')
-            ->join('Bilder','Bilder.ID','=','Mahlzeit_hat_Bild.Bild_ID')
-            ->join('Preise','Mahlzeiten.ID','=','Preise.Mahlzeiten_ID')
-            ->where('Mahlzeit_hat_Bild.Mahlzeiten_ID','=',$id)
-//          ->select(Mahlz)
-          ->first();
-
-      $zutaten = DB::table('Mahl_enthaelt_zutat')->join('Zutaten','Mahl_enthaelt_zutat.Zutat_ID'
-          ,'=','Zutaten.ID')->where('Mahlzeit_ID','=',$id)->get();
-
-
-        return view('detail',['mahlzeiten' => $mahlzeiten, 'zutaten'=> $zutaten, 'id' => $id]);
-    }
-
-    public function auth($id)
+    public function auth()
     {
 //        "_token" => "Vokg1mPthkO7zgx5jOOpgO7wrJWWTcdbP1gDNXb9"
 //      "benutzer" => "dsad"
@@ -38,11 +17,12 @@ class DetailsController extends Controller
 
         // \request()->benutzer
         // \request()->password
+    dd(\request());
         session_start();
         $benutzer = DB::table('Benutzer')->where('Benutzer.Nutzername', '=',\request()->benutzer)
             ->select('Hash', 'Nummer')->first();
 
-        if ( \request()->action == 'Anmelden' AND  $benutzer != null AND password_verify(\request()->password, $benutzer->Hash)){
+        if (password_verify(\request()->password, $benutzer->Hash)){
 
 //            $sql = DB::select('CALL UserRole(\request()->Nummer, @role');
 //            $sql = DB::select('SELECT @role');
@@ -50,8 +30,9 @@ class DetailsController extends Controller
 //            $sql = DB::select('UPDATE Benutzer B SET B.`LetzterLogin` = NOW() WHERE B.Nummer = {{/request->Nummer}}');
             $_SESSION['user'] = $_POST['benutzer'];
 //            $_SESSION['role'] = $row2["@role"];
+            dd($_POST);
 
-        }else if(\request()->action == 'Abmelden'){
+        }else if($_POST['action'] == 'Abmelden'){
             unset($_SESSION['user']);
 //            unset($_SESSION['role']);
         } else {
@@ -61,7 +42,7 @@ class DetailsController extends Controller
             $_SESSION['angemeldet'] = 0;
         }
 
-        return redirect()->back();
+        return redirect()->action('DetailsController@index');
     }
 
 }
